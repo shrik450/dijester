80835bfe96465fb9a6d9fc14bd30e73a49127680
Shrikanth Upadhayaya <shrik450@gmail.com>
2022-05-21 16:25:30 +0530

    Set up logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+use env_logger::{Builder, Env, Target};
+use log;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(parse(from_os_str))]
+    config_file: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    Builder::from_env(Env::default())
+        .target(Target::Stdout)
+        .init();
+
+    let args = Cli::parse();
+
+    log::info!("Loading config file: {:#?}", args.config_file);
 }
